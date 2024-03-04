b5581099254e9bfd42eff7262941a81a8fb5679f
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-03-04 05:03:53 +0400

    MADE CHANGIES

--- React/lecture_4/src/App.jsx
+++ React/lecture_4/src/App.jsx
@@ -20,32 +20,71 @@ function App() {
   // });
 
   const [input, SetInput] = useState("");
-  const [output, SetOutput] = useState([]);
-  // function ChangesInput(params) {
-  //   SetInput(params.target.value);
-  // }
+  const [output, SetOutput] = useState([
+    // {
+    //   id: 1,
+    //   text: "First_Task",
+    //   complited: false,
+    // },
+  ]);
 
   function AddedInput() {
-    SetOutput((Something) => [...Something, input]);
+    SetOutput((anyvalue) => [
+      ...anyvalue,
+      { id: anyvalue.length + 1, text: input, complited: false },
+    ]);
+    localStorage.setItem("ToDo", JSON.stringify(output));
   }
+
+  function Deltetask(ToDo_tasks) {
+    SetOutput((anyvalue) =>
+      anyvalue.filter((param) => param.text != ToDo_tasks)
+    );
+  }
+
+  function ChekedToDo(ToDo_id) {
+    SetOutput((anyvalue) =>
+      anyvalue.map((param) => {
+        return param.id === ToDo_id
+          ? { ...param, complited: !param.complited }
+          : param;
+      })
+    );
+  }
+
+  const [dark, SetDark] = useState(false)
+
+
 
   return (
     <>
-      <input type="text" onChange={(e) => SetInput(e.target.value)}></input>
+
+      {/* <input type="text" onChange={(e) => SetInput(e.target.value)}></input>
       <button onClick={AddedInput}>Click me</button>
       <div>
-        {output.map((item) => {
+        {output.map((todo) => {
           return (
             <>
               <div style={{ display: "flex", gap: 15 }}>
-                <li style={{ listStyle: "none" }}>{item}</li>
-                <input type="checkbox" />
-                <button>Delete</button>
+                <li
+                  style={{
+                    listStyle: "none",
+                    textDecoration: todo.complited ? "line-through " : "none",
+                  }}
+                >
+                  {todo.text}
+                </li>
+                <input
+                  type="checkbox"
+                  checked={todo.complited}
+                  onChange={() => ChekedToDo(todo.id)}
+                />
+                <button onClick={() => Deltetask(todo.text)}>Delete</button>
               </div>
             </>
           );
         })}
-      </div>
+      </div> */}
     </>
   );
 }
